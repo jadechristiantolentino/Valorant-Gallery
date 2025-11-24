f17fd841a86d143e0bc26e5a3b4a5f8ccdf260a9
jadechristiantolentino <jade03space@gmail.com>
2025-11-25 01:31:45 +0800

    update the js,index,and css

--- character.js
+++ character.js
@@ -1,7 +1,14 @@
 var characters = [
-    { "name": "Jett", "image": "images/jett.jpg"},
-    { "name": "Omen", "image": "images/omen.jpg"},
-    { "name": "Clove", "image": "images/clove.jpg"},
-    { "name": "Brimstone", "image": "images/brimstone.jpg" },
-    { "name": "Yoru", "image": "images/yoru.jpg" }
+    { "name": "Jett", "image": "images/jett.jpg", "role": "Duelist" },
+    { "name": "Omen", "image": "images/omen.jpg", "role": "Controller" },
+    { "name": "Clove", "image": "images/clove.jpg", "role": "Controller" },
+    { "name": "Brimstone", "image": "images/brimstone.jpg", "role": "Controller" },
+    { "name": "Yoru", "image": "images/yoru.jpg", "role": "Duelist" },
+    { "name": "Iso", "image": "images/iso.jpg", "role": "Duelist" },
+    { "name": "Raze", "image": "images/raze.jpg", "role": "Duelist" },
+    { "name": "Neon", "image": "images/neon.jpg", "role": "Duelist" },
+    { "name": "Reyna", "image": "images/reyna.jpg", "role": "Duelist" },
+    { "name": "Fade", "image": "images/fade.jpg", "role": "Initiator" },
+    { "name": "Chamber", "image": "images/chamber.jpg", "role": "Sentinel" },
+    { "name": "Gekko", "image": "images/gekko.jpg", "role": "Initiator" }
 ];
