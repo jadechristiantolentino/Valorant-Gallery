cdbc7f4f6a0cc1a20604e5ed825100dd68851598
jadechristiantolentino <jade03space@gmail.com>
2025-11-24 23:12:45 +0800

    update index, add .js and add more picture

---
+++ character.js
@@ -0,0 +1,7 @@
+var characters = [
+    { "name": "Jett", "image": "images/jett.jpg"},
+    { "name": "Omen", "image": "images/omen.jpg"},
+    { "name": "Clove", "image": "images/clove.jpg"},
+    { "name": "Brimstone", "image": "images/brimstone.jpg" },
+    { "name": "Yoru", "image": "images/yoru.jpg" }
+];
